0565d19d78a5bf565be2f3096e6470e4f779b219
Fredmark Ivan D. Dizon <114529635+Kuraiyume@users.noreply.github.com>
2024-09-05 14:34:09 +0800

    Update xiore.c

--- xiore.c
+++ xiore.c
@@ -1,6 +1,6 @@
 /*
  * Xiore Cipher
- * Author: Z33phyr
+ * Author: Kuraiyume
  *
  * The Xiore Cipher is a proprietary symmetric encryption algorithm designed for robust data security. 
  * It operates with a 256-bit key and employs multiple rounds of encryption to secure plaintext data.
